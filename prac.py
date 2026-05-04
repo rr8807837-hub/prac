3700e8b7318bfa557a973fa755d9c065d3158ad7
rr8807837-hub <rr8807837@gmail.com>
2026-05-04 19:15:30 +0530

    Refactor Streamlit app layout and input handling

--- prac.py
+++ prac.py
@@ -1,16 +1,20 @@
 import streamlit as st
+st.set_page_config(page_title="Streamlit Practice", page_icon=":smile:",initial_sidebar_state="expanded")
+if "gen" not in st.session_state:
+    st.session_state.gen=False
 st.markdown("""
    <style>
    header[data-testid="stHeader"]{
-       visibility:hidden;
+       display:none;
     }
    footer{display=none;}
    #MainMenu{
        visibility:hidden;
    }
-   section[data-testid="stSidebar"]{
-       visibility:block !important;
-   } 
    <\style>         
 """,unsafe_allow_html=True)
 st.sidebar.text_input("name")
+name=st.text_input("name", key="text")
+button=st.button("submit" ,key="sub")
+if st.button:
+    st.write(name)
