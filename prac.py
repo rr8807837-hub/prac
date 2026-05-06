6795f34ceab4b86cd41018e36ce25daee90d1c8a
rr8807837-hub <rr8807837@gmail.com>
2026-05-06 13:06:28 +0530

    Implement MongoDB connection and chat input
    Added MongoDB connection and input handling for chat messages.

--- prac.py
+++ prac.py
@@ -1,20 +1,31 @@
 import streamlit as st
-st.set_page_config(page_title="Streamlit Practice", page_icon=":smile:",initial_sidebar_state="expanded")
+import pymongo
+from pymongo import MongoClient
+import webbrowser
+
 if "gen" not in st.session_state:
-    st.session_state.gen=False
+    st.session_state.gen=""
+# MongoDB connection
+client = MongoClient("mongodb+srv://rr8807837_db_user:raja44raja@cluster0.jstxelp.mongodb.net/")
+db = client["chat"]
+collection = db["message"]
+st.set_page_config(page_title="mongo",layout="wide",page_icon=":mango:")
+st.title("MongoDB connection")
+st.session_state.chat = st.text_input("enter here" ,key="chatinput")
+st.latex(r"a+b=c") 
+if st.button("save"):
+    chat=collection.insert_one({"message":st.session_state.chat})
+    if st.success("saved into mongoDB"):
+        st.switch_page("pages/p.py")
 st.markdown("""
-   <style>
-   header[data-testid="stHeader"]{
-       display:none;
-    }
-   footer{display=none;}
-   #MainMenu{
-       visibility:hidden;
-   }
-   <\style>         
-""",unsafe_allow_html=True)
-st.sidebar.text_input("name")
-name=st.text_input("name", key="text")
-button=st.button("submit" ,key="sub")
-if st.button:
-    st.write(name)
+            <style>
+            .stButton>button{
+                border-radius:10px;
+                background-color:red;
+                margin-left:440px;
+            }
+            </style>
+            """,unsafe_allow_html=True)        
+t1=st.toggle("login")
+if  t1:
+    st.toggle("signup")      
